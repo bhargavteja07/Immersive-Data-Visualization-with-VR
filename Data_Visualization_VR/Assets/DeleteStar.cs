fafbc9364e10896219cd691970a1fe9a9d47fc48
srujan belde <srujanbelde18@gmail.com>
2017-10-20 13:22:56 +0530

    delete star

---
+++ Data_Visualization_VR/Assets/DeleteStar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteStar : MonoBehaviour {
+    public string starname;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
